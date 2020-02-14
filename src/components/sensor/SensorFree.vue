489642ea4524fd47c70f699daf7bc739783e3c44
vol4tim <vol4tim@gmail.com>
2020-02-14 11:56:59 +0400

    add service

--- src/components/sensor/SensorFree.vue
+++ src/components/sensor/SensorFree.vue
@@ -3,47 +3,57 @@
     <div v-if="ready">
       <h4>
         {{ $t("sensor.statusAgent") }}:
-        <template
-          v-if="log.length === 0"
-        >{{ $t("sensor.notStatusAgent") }}</template>
+        <template v-if="log.length === 0">{{
+          $t("sensor.notStatusAgent")
+        }}</template>
         <template v-else>
           {{ $t("sensor.yesStatusAgent") }}
           {{ log[log.length - 1].time }}
         </template>
       </h4>
-      <section>
+      <section
+        v-if="model !== 'Qmczm9hw8SjGmtx55t6MJPQTtXQDuS9grqaTb18Sv8b6pm'"
+      >
         <div class="input-size--md">
           <RButton v-if="isRequest" full green disabled>
-            {{
-            $t("sensor.requested")
-            }}
+            {{ $t("sensor.requested") }}
           </RButton>
           <RButton v-else @click.native="sendMsgDemand" full green>
-            {{
-            $t("sensor.isRequest")
-            }}
+            {{ $t("sensor.isRequest") }}
           </RButton>
         </div>
       </section>
       <RWindow v-if="log.length > 0" id="window-sensornetwork-requests">
         <template slot="header">
           <span>
-            {{ $t("sensor.requests") }} ({{log.length}})
+            {{ $t("sensor.requests") }} ({{ log.length }})
             <RButton
               @click.native="clear"
               style="background:none;color:#03a5ed;border:2px solid #03a5ed;padding-top:2px;padding-bottom:2px;margin-left:15px;"
-            >{{ $t("sensor.clear") }}</RButton>
+              >{{ $t("sensor.clear") }}</RButton
+            >
           </span>
         </template>
 
         <Pagination :listData="log.slice().reverse()">
           <template v-slot:default="props">
             <RCard>
-              <Message :item="props.item" :lighthouse="lighthouse" :model="model" :agent="agent" />
+              <Message
+                :item="props.item"
+                :lighthouse="lighthouse"
+                :model="model"
+                :agent="agent"
+              />
             </RCard>
           </template>
         </Pagination>
       </RWindow>
+      <RCard
+        v-else-if="model === 'Qmczm9hw8SjGmtx55t6MJPQTtXQDuS9grqaTb18Sv8b6pm'"
+      >
+        <span class="align-vertical">{{ $t("sensor.wait") }}</span>
+        <div class="loader-ring align-vertical m-l-10"></div>
+      </RCard>
     </div>
   </div>
 </template>
@@ -79,33 +89,41 @@ export default {
       const data = history.getData(this.storeKey);
       this.log = data;
       data.forEach((item, index) => {
-        parseResult(item.resultHash).then(result => {
-          Vue.set(this.log, index, {
-            ...this.log[index],
-            status: 3,
-            result: result
-          });
-          history.addItem(
-            this.storeKey,
-            {
+        if (
+          Object.prototype.hasOwnProperty.call(item, "status") &&
+          item.status >= 2
+        ) {
+          parseResult(item.resultHash).then(result => {
+            Vue.set(this.log, index, {
               ...this.log[index],
               status: 3,
               result: result
-            },
-            index
-          );
-        });
+            });
+            history.addItem(
+              this.storeKey,
+              {
+                ...this.log[index],
+                status: 3,
+                result: result
+              },
+              index
+            );
+          });
+        }
       });
 
       this.$robonomics.onDemand(this.model, msg => {
         console.log("demand", msg);
       });
       this.$robonomics.onResult(msg => {
-        console.log("open", msg);
         // const sender = msg.recovery();
         const sender = this.$robonomics.account.recoveryMessage(msg);
+        console.log("open", sender, msg);
 
-        if (msg.liability === "0x0000000000000000000000000000000000000000") {
+        if (
+          sender.toLowerCase() === this.agent.toLowerCase() &&
+          msg.liability === "0x0000000000000000000000000000000000000000"
+        ) {
           const item = {
             status: 1,
             time: new Date().toLocaleString()
