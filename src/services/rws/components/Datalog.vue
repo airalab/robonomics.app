5b367d3dc54b23393b6fa9f3cacc8c10c64799a3
vol4tim <vol4tim@gmail.com>
2020-08-20 13:24:11 +0400

    upd rws module

--- src/services/rws/components/Datalog.vue
+++ src/services/rws/components/Datalog.vue
@@ -1,17 +1,36 @@
 <template>
   <div>
+    <hr />
+    <div class="row">
+      <div class="col-md-6" style="text-align: center;">
+        available
+        <span style="font-size: 20px; font-weight: bold;">
+          {{ availableTps }}
+        </span>
+        tps
+      </div>
+      <div class="col-md-6" style="text-align: center;">
+        used
+        <span style="font-size: 20px; font-weight: bold;">
+          {{ usedTps }}
+        </span>
+        tps
+      </div>
+    </div>
     <h3>Datalog history by account</h3>
-    <p>
+    <!-- <p>
       Count all tx:
       <b>{{ count }}</b>
-    </p>
-    <h4>Last tx</h4>
+    </p> -->
+    <h4>Last 10 tx</h4>
     <template v-if="!proccess">
       <ul v-if="count > 0">
         <li v-for="(item, k) in last" :key="k">
-          <small>{{new Date(Number(item[0])).toLocaleString()}}</small>
+          <small>{{ new Date(Number(item[0])).toLocaleString() }}</small>
           <br />
-          <a :href="`https://ipfs.io/ipfs/${item[1]}`" target="_blank">{{item[1]}}</a>
+          <a :href="`https://ipfs.io/ipfs/${item[1]}`" target="_blank">{{
+            item[1]
+          }}</a>
         </li>
       </ul>
       <div v-else>not found tx</div>
@@ -21,16 +40,26 @@
 </template>
 
 <script>
+import moment from "moment";
 import { getInstance, toIpfsHash } from "../utils/substrate";
 
 export default {
-  props: ["account"],
+  props: ["account", "amount"],
   data() {
     return {
       last: [],
       count: 0,
+      countMonth: 0,
       proccess: false
     };
+  },
+  computed: {
+    availableTps: function () {
+      return this.$options.filters.fromWei(this.amount, 18);
+    },
+    usedTps: function () {
+      return Math.round((this.countMonth / 86400) * 1000) / 1000;
+    }
   },
   mounted() {
     this.run();
@@ -52,6 +81,12 @@ export default {
             await substrate.query.datalog.datalog(this.account)
           ).toArray();
           this.count = log.length;
+
+          const filterMonth = Number(moment().subtract(30, "day").format("x"));
+          this.countMonth = log.reverse().filter((item) => {
+            return Number(item[0]) > filterMonth;
+          }).length;
+
           this.last = log
             .slice(-10)
             .reverse()
--- src/services/rws/components/Subscribe.vue
+++ src/services/rws/components/Subscribe.vue
@@ -2,8 +2,11 @@
   <fragment>
     <div class="row">
       <div class="col-md-6" style="text-align: center;">
-        <span style="font-size: 20px; font-weight: bold;">{{ myTps }}</span>&nbsp;
-        <span>{{ status }}</span>&nbsp;
+        <span style="font-size: 20px; font-weight: bold;">{{
+          stake.amount | fromWei(18, "RWS")
+        }}</span
+        >&nbsp; <span>{{ status }}</span
+        >&nbsp;
         <!-- <span
           v-if="stake.status == 2 && current_block - stake.last_update <= lock_duration"
         >{{Number(lock_duration)-(Number(current_block)-Number(stake.last_update))}} blocks</span>-->
@@ -18,10 +21,13 @@
             (stake.status == 2 &&
               current_block - stake.last_update <= lock_duration)
           "
-        >withdraw</RButton>
+          >withdraw</RButton
+        >
       </div>
       <div class="col-md-6" style="text-align: center;">
-        <span style="font-size: 20px; font-weight: bold;">{{ myBalanceFormat }}</span>
+        <span style="font-size: 20px; font-weight: bold;">{{
+          myBalanceFormat
+        }}</span>
         tokens
         <br />
         <p>
@@ -29,18 +35,25 @@
           <a
             href="https://app.uniswap.org/#/swap?inputCurrency=0x7de91b204c1c737bcee6f000aaa6569cf7061cb7&outputCurrency=0x08ad83d779bdf2bbe1ad9cc0f78aa0d24ab97802"
             target="_blank"
-          >trade</a>
+            >trade</a
+          >
           or
           <RButton
             size="sm"
             @click="toggleFormActivate = true"
             :disabled="toggleFormActivate || myBalance <= 0"
-          >activate</RButton>
+            >activate</RButton
+          >
         </p>
       </div>
     </div>
 
-    <Activate v-if="toggleFormActivate" :account="stake.account" @account="handleAccount" />
+    <Activate v-if="toggleFormActivate" :account="stake.account" />
+    <!-- <Activate
+      v-if="toggleFormActivate"
+      :account="stake.account"
+      @account="handleAccount"
+    /> -->
     <Deactivate
       v-else-if="stake.amount > 0"
       :stake="stake"
@@ -48,7 +61,9 @@
       :lock_duration="lock_duration"
     />
 
-    <Datalog v-if="account" :account="account" />
+    <div v-if="stake.account && stake.amount > 0">
+      <Datalog ref="datalog" :account="stake.account" :amount="stake.amount" />
+    </div>
   </fragment>
 </template>
 
@@ -73,7 +88,7 @@ export default {
       toggleFormActivate: true,
       current_block: 0,
       lock_duration: 0,
-      account: "",
+      // account: "",
       stake: {
         last_update: "",
         status: 0,
@@ -116,15 +131,12 @@ export default {
     },
     myBalanceFormat: function () {
       return this.balanceFormat(config.RWS, this.$robonomics.account.address);
-    },
-    myTps: function () {
-      return this.$options.filters.fromWei(this.stake.amount, 18, "tps");
     }
   },
   methods: {
-    handleAccount(account) {
-      this.account = account;
-    },
+    // handleAccount(account) {
+    //   this.account = account;
+    // },
     watchEvents(fromBlock) {
       const contract = new this.$robonomics.web3.eth.Contract(
         SubscriptionAbi,
@@ -168,7 +180,7 @@ export default {
               amount: r.amount,
               account: encodeAddress(r.account, 32)
             };
-            this.account = this.stake.account;
+            // this.account = this.stake.account;
           }
         });
     }
