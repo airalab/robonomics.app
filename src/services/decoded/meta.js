417d1b4167bc83061891ece23edae44cb06ca95d
vol4tim <vol4tim@gmail.com>
2021-05-26 11:09:00 +0400

    add module decoded

---
+++ src/services/decoded/meta.js
@@ -0,0 +1,23 @@
+export default {
+  en: {
+    name: "Polkadot Decoded Demo",
+    desc:
+      "IoT under Polkadot control decentralized application with integration of web3 services"
+  },
+  ru: {
+    name: "Polkadot Decoded Demo",
+    desc:
+      "IoT under Polkadot control decentralized application with integration of web3 services"
+  },
+  link: "https://dapp.robonomics.network/decoded/",
+  target: true,
+  img: require("./logo.jpg"),
+  by: [
+    {
+      link: "https://airalab.org/",
+      label: "Airalab"
+    }
+  ],
+  token: null,
+  disabled: false
+};
