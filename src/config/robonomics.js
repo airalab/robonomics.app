fbc67233362860383162e100f022b69d3d9b6756
vol4tim <vol4tim@gmail.com>
2022-01-26 14:49:31 +0400

    update ipfs version

--- src/config/robonomics.js
+++ src/config/robonomics.js
@@ -1,16 +1,7 @@
 let config = {
   ipfs: {
     fallback: {
-      repo: "ipfs/robonomics/4",
-      relay: {
-        enabled: true,
-        hop: {
-          enabled: true
-        }
-      },
-      // EXPERIMENTAL: {
-      //   pubsub: true
-      // },
+      repo: "ipfs/robonomics/5",
       config: {
         Addresses: {
           Swarm: [
@@ -20,12 +11,6 @@ let config = {
           ]
         },
         Bootstrap: [
-          "/dns4/ams-1.bootstrap.libp2p.io/tcp/443/wss/ipfs/QmSoLer265NRgSp2LA3dPaeykiS1J6DifTC88f5uVQKNAd",
-          "/dns4/lon-1.bootstrap.libp2p.io/tcp/443/wss/ipfs/QmSoLMeWqB7YGVLJN3pNLQpmmEk35v6wYtsMGLzSr5QBU3",
-          "/dns4/nyc-1.bootstrap.libp2p.io/tcp/443/wss/ipfs/QmSoLueR4xBeUbY9WZ9xGUUxunbKWcrNFTDAadQJmocnWm",
-          "/dns4/nyc-2.bootstrap.libp2p.io/tcp/443/wss/ipfs/QmSoLV4Bbm51jM9C4gDYZQ9Cy3U6aXMJDAbzgu2fzaDs64",
-          "/dns4/node0.preload.ipfs.io/tcp/443/wss/ipfs/QmZMxNdpMkewiVZLMRxaNxUeZpDUb34pWjZ1kZvsd16Zic",
-          "/dns4/node1.preload.ipfs.io/tcp/443/wss/ipfs/Qmbut9Ywz9YEDrz8ySBSgWyJk41Uvm2QJPhwDJzJyGFsD6",
           "/dns4/1.pubsub.aira.life/tcp/443/wss/ipfs/QmdfQmbmXt6sqjZyowxPUsmvBsgSGQjm4VXrV7WGy62dv8",
           "/dns4/2.pubsub.aira.life/tcp/443/wss/ipfs/QmPTFt7GJ2MfDuVYwJJTULr6EnsQtGVp8ahYn9NSyoxmd9",
           "/dns4/3.pubsub.aira.life/tcp/443/wss/ipfs/QmWZSKTEQQ985mnNzMqhGCrwQ1aTA6sxVsorsycQz9cQrw"
@@ -36,7 +21,7 @@ let config = {
     // cdn: "https://unpkg.com/ipfs@0.40.0/dist/index.min.js",
     // cdn: "https://unpkg.com/ipfs@0.48.1/dist/index.min.js",
     // cdn: "https://unpkg.com/ipfs@0.46.0/dist/index.min.js",
-    cdn: "https://unpkg.com/ipfs@0.52.0/dist/index.min.js",
+    cdn: "https://cdn.jsdelivr.net/npm/ipfs@0.61.0/index.min.js",
     permission: [
       "id",
       "files.cat",
