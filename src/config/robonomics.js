3e8f5a084eb9af54ed0a118510ebe3d9e2cac0ea
vol4tim <vol4tim@gmail.com>
2022-02-24 14:04:55 +0400

    disabled 1.pubsub.aira.life

--- src/config/robonomics.js
+++ src/config/robonomics.js
@@ -5,13 +5,13 @@ let config = {
       config: {
         Addresses: {
           Swarm: [
-            "/dns4/1.webrtcstar.aira.life/tcp/443/wss/p2p-webrtc-star/",
+            // "/dns4/1.webrtcstar.aira.life/tcp/443/wss/p2p-webrtc-star/",
             "/dns4/2.webrtcstar.aira.life/tcp/443/wss/p2p-webrtc-star/",
             "/dns4/3.webrtcstar.aira.life/tcp/443/wss/p2p-webrtc-star/"
           ]
         },
         Bootstrap: [
-          "/dns4/1.pubsub.aira.life/tcp/443/wss/ipfs/QmdfQmbmXt6sqjZyowxPUsmvBsgSGQjm4VXrV7WGy62dv8",
+          // "/dns4/1.pubsub.aira.life/tcp/443/wss/ipfs/QmdfQmbmXt6sqjZyowxPUsmvBsgSGQjm4VXrV7WGy62dv8",
           "/dns4/2.pubsub.aira.life/tcp/443/wss/ipfs/QmPTFt7GJ2MfDuVYwJJTULr6EnsQtGVp8ahYn9NSyoxmd9",
           "/dns4/3.pubsub.aira.life/tcp/443/wss/ipfs/QmWZSKTEQQ985mnNzMqhGCrwQ1aTA6sxVsorsycQz9cQrw"
         ]
