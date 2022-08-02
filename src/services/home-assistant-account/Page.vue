e9047df27dec97bbf10710f69d50f70b00e17e89
vol4tim <vol4tim@gmail.com>
2022-08-02 13:35:28 +0400

    update page for create account ha

--- src/services/home-assistant-account/Page.vue
+++ src/services/home-assistant-account/Page.vue
@@ -26,67 +26,83 @@
               Enter address of subscription owner
             </robo-text>
             <robo-input v-model="ha" />
+            <robo-text weight="light">
+              Enter address of administrator Home Assistant
+            </robo-text>
+            <robo-input v-model="subAdmin" />
           </robo-list-item>
 
-          <robo-list-item>
-            <robo-text weight="light">
-              Enter address of the account that encrypted the password
-            </robo-text>
-            <robo-input
-              placeholder="Place address here"
-              offset="x1"
-              v-model="subAdmin"
-            />
-          </robo-list-item>
-
-          <robo-list-item>
-            <robo-card-title size="3">
-              Get Home Assistant password:
-            </robo-card-title>
-
-            <template v-if="isSubscription">
-              <robo-text weight="bold">crypted password:</robo-text>
-              <robo-text v-if="secret" break gap size="small">
-                {{ secret }}
-              </robo-text>
-              <robo-loader v-else />
-            </template>
-
-            <robo-text weight="light">
-              Enter the seed phrase of your account to decrypt the password
-            </robo-text>
-            <robo-input
-              placeholder="Place seed here"
-              offset="x1"
-              v-model="uri"
-              type="password"
-            />
-            <robo-button
-              @click="decrypt"
-              outlined
-              :disabled="!validateUri || !secret || !subAdmin"
+          <template v-if="isSubscription">
+            <div
+              v-if="encryptedPasswords.length > 0"
+              style="margin-bottom: 20px"
             >
-              Decrypt password
-            </robo-button>
-          </robo-list-item>
+              <robo-button
+                @click="form = 'registration'"
+                :disabled="form === 'registration'"
+                >Registration</robo-button
+              >
+              &nbsp;
+              <robo-button
+                @click="form = 'recovery'"
+                :disabled="form === 'recovery'"
+                >Recovery</robo-button
+              >
+            </div>
 
-          <robo-list-item>
-            <robo-card-title size="3">
-              Sign in your Home assistant account using login and password for
-              autorization:
-            </robo-card-title>
+            <robo-list-item v-if="form === 'recovery'">
+              <robo-text weight="light">Your passwords</robo-text>
+              <div v-for="(item, k) in encryptedPasswords" :key="k">
+                <b>{{ item.date }}</b> |
+                <span v-if="item.password">{{ item.password }}</span>
+                <span v-else>{{ item.encrypt.substring(0, 5) }}...</span>
+              </div>
+            </robo-list-item>
 
-            <template v-if="password">
-              <robo-text weight="bold">login:</robo-text>
-              <robo-text gap size="small">
-                <robo-account-polkadot copy />
+            <robo-list-item>
+              <robo-text weight="light">
+                Enter the seed phrase of your account to encrypt the password
               </robo-text>
+              <robo-input
+                placeholder="Place seed here"
+                offset="x1"
+                v-model="uri"
+                type="password"
+              />
+            </robo-list-item>
 
-              <robo-text weight="bold">password:</robo-text>
-              <robo-text break copy gap size="small">{{ password }}</robo-text>
+            <template v-if="validateUri">
+              <robo-button v-if="form === 'recovery'" @click="recoveryPassword">
+                recovery
+              </robo-button>
+
+              <robo-list-item v-if="form === 'registration'">
+                <robo-text weight="light">Enter password</robo-text>
+                <robo-input v-model="passwordInput" type="password" />
+              </robo-list-item>
+
+              <robo-list-item v-if="process">
+                <robo-text weight="bold" size="medium">
+                  Please wait <robo-loader />
+                </robo-text>
+              </robo-list-item>
+              <robo-button
+                v-if="form === 'registration'"
+                @click="savePassword"
+                :disabled="!passwordForAdmin || !passwordForRecovery || process"
+              >
+                Registration
+              </robo-button>
+
+              <robo-list-item style="margin-top: 20px">
+                <robo-card-title size="3">
+                  Sign in your Home assistant account using login and password
+                  for autorization:
+                </robo-card-title>
+                <robo-text gap>http://192.168.xx.xx:xxxx/</robo-text>
+              </robo-list-item>
             </template>
-            <robo-text gap>http://192.168.xx.xx:xxxx/</robo-text>
-          </robo-list-item>
+          </template>
         </robo-list>
       </robo-card-section>
     </robo-card>
@@ -96,7 +112,7 @@
 <script>
 import robonomics from "../../robonomics";
 import { Keyring } from "@polkadot/keyring";
-import { u8aToString, u8aToHex } from "@polkadot/util";
+import { u8aToString, u8aToHex, hexToU8a } from "@polkadot/util";
 import { decodeAddress, encodeAddress } from "@polkadot/util-crypto";
 
 export default {
@@ -107,13 +123,15 @@ export default {
       isSubscription: false,
       sender: null,
       unsubscribeAccount: null,
-      unsubscribeDatalog: null,
       error: null,
       process: false,
-      log: [],
-      secret: "",
-      password: "",
-      uri: ""
+      message: "",
+      uri: "",
+      passwordInput: "",
+      passwordForAdmin: "",
+      passwordForRecovery: "",
+      encryptedPasswords: [],
+      form: "registration"
     };
   },
   computed: {
@@ -141,44 +159,97 @@ export default {
   async created() {
     if (robonomics.accountManager.account) {
       this.sender = robonomics.accountManager.account.address;
+      this.findPassword();
       this.hasSubscription();
-      this.read();
     }
     this.unsubscribeAccount = robonomics.accountManager.onChange((account) => {
       this.sender = account.address;
+      this.findPassword();
       this.hasSubscription();
-      this.read();
-    });
-
-    this.unsubscribeDatalog = robonomics.datalog.on({}, (result) => {
-      result
-        .filter((item) => item.account.toHuman() === this.ha)
-        .forEach((item) => {
-          try {
-            const json = JSON.parse(item.data.toHuman());
-            if (json.address && json.password && json.address === this.sender) {
-              this.secret = json.password;
-            }
-            // eslint-disable-next-line no-empty
-          } catch (_) {}
-        });
     });
   },
   watch: {
     ha() {
       this.hasSubscription();
-      this.read();
+    },
+    passwordInput() {
+      this.passwordForRecovery = this.encrypt(
+        this.passwordInput,
+        this.account.address
+      );
+      this.passwordForAdmin = this.encrypt(this.passwordInput, this.subAdmin);
     }
   },
   unmounted() {
     if (this.unsubscribeAccount) {
       this.unsubscribeAccount();
     }
-    if (this.unsubscribeDatalog) {
-      this.unsubscribeDatalog();
-    }
   },
   methods: {
+    async findPassword() {
+      this.encryptedPasswords = [];
+      const log = await robonomics.datalog.read(this.sender);
+      for (const item of log) {
+        try {
+          const json = JSON.parse(item[1].toHuman());
+          if (json.admin && json.user) {
+            this.encryptedPasswords.push({
+              date: new Date(item[0].toNumber()).toLocaleString(),
+              encrypt: json.user,
+              password: ""
+            });
+          }
+          // eslint-disable-next-line no-empty
+        } catch (_) {}
+      }
+    },
+    recoveryPassword() {
+      for (const k in this.encryptedPasswords) {
+        this.encryptedPasswords[k].password = this.decrypt(
+          this.encryptedPasswords[k].encrypt,
+          this.account.address
+        );
+      }
+    },
+    decrypt(hex, address) {
+      const decryptMessage = this.account.decryptMessage(
+        hexToU8a(hex),
+        u8aToHex(decodeAddress(address))
+      );
+      return u8aToString(decryptMessage);
+    },
+    encrypt(password, address) {
+      const decryptMessage = this.account.encryptMessage(
+        password,
+        u8aToHex(decodeAddress(address))
+      );
+      return u8aToHex(decryptMessage);
+    },
+    async savePassword() {
+      this.error = null;
+      this.process = true;
+      this.tx = false;
+      this.message = "";
+      try {
+        robonomics.accountManager.useSubscription(this.ha);
+        const tx = await robonomics.datalog.write(
+          JSON.stringify({
+            admin: this.passwordForAdmin,
+            user: this.passwordForRecovery
+          })
+        );
+        const resultTx = await robonomics.accountManager.signAndSend(tx);
+        console.log(resultTx);
+        this.process = false;
+        this.tx = true;
+        this.message = `Saved to block ${resultTx.blockNumber}`;
+      } catch (e) {
+        console.log(e);
+        this.error = e.message;
+        this.process = false;
+      }
+      robonomics.accountManager.useSubscription(false);
+    },
     async hasSubscription() {
       const devices = await robonomics.rws.getDevices(this.ha);
       if (devices.includes(this.sender)) {
@@ -186,25 +257,6 @@ export default {
         return;
       }
       this.isSubscription = false;
-    },
-    async read() {
-      const log = await robonomics.datalog.read(this.ha);
-      for (const item of log) {
-        try {
-          const json = JSON.parse(item[1].toHuman());
-          if (json.address && json.password && json.address === this.sender) {
-            this.secret = json.password;
-          }
-          // eslint-disable-next-line no-empty
-        } catch (_) {}
-      }
-    },
-    decrypt() {
-      const decryptMessage = this.account.decryptMessage(
-        this.secret,
-        u8aToHex(decodeAddress(this.subAdmin))
-      );
-      this.password = u8aToString(decryptMessage);
     }
   }
 };
