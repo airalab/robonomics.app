a554e8bbea1e10c2f46aa5b1c060165353bd7c06
vol4tim <vol4tim@gmail.com>
2022-08-22 15:42:46 +0400

    update page HomeAssistant account

---
+++ src/services/home-assistant-account/Hello.vue
@@ -0,0 +1,25 @@
+<template>
+  <robo-section>
+    <robo-card>
+      <robo-card-label>
+        <robo-card-label-section>Overview</robo-card-label-section>
+      </robo-card-label>
+
+      <robo-card-section>
+        <robo-section offset="x1">
+          <robo-text weight="normal-italic" offset="x1" hyphen
+            >Any parachain account added in Robonomics subscription by
+            Subscription Owner may interact with smart home in Home Assistant
+            local panel. This is the way to add users to Home assistant without
+            third party servers. Only user can access to all smart home data &
+            services by encrypting it with seed phrase.</robo-text
+          >
+          <robo-text weight="normal-italic" offset="x1" hyphen
+            >Here you may create or restore Home Assistant password for user
+            added in subscription.</robo-text
+          >
+        </robo-section>
+      </robo-card-section>
+    </robo-card>
+  </robo-section>
+</template>
---
+++ src/services/home-assistant-account/Note.vue
@@ -0,0 +1,42 @@
+<template>
+  <robo-section>
+    <robo-section
+      backColor="rgb(16, 202, 249)"
+      backImage="images/ha-logo.png"
+      backSize="contain"
+      textColor="#ffffff"
+      offset="x0"
+      backPosition="100% 10%"
+    >
+      <robo-card-title size="2" offset="x0"
+        >Home Assistant local</robo-card-title
+      >
+    </robo-section>
+
+    <robo-section backColor="rgb(16, 202, 249)" textColor="#ffffff" offset="x1">
+      <robo-section offset="x05">
+        <robo-text weight="bold" inline>URL: </robo-text>
+        <robo-text weight="normal-italic" inline
+          >http://YOUR_LOCAL_ADDRESS:8123/ [Get exact address from your
+          administrator or read
+          <robo-link
+            href="https://wiki.robonomics.network/docs/home-assistant-begin/"
+            >our wiki guide</robo-link
+          >]</robo-text
+        >
+      </robo-section>
+      <robo-section offset="x05">
+        <robo-text weight="bold" inline>Login: </robo-text>
+        <robo-text weight="normal-italic" inline
+          >your Parachain Address added in subscription</robo-text
+        >
+      </robo-section>
+      <robo-section offset="x05">
+        <robo-text weight="bold" inline>Password: </robo-text>
+        <robo-text weight="normal-italic" inline
+          >use password created here</robo-text
+        >
+      </robo-section>
+    </robo-section>
+  </robo-section>
+</template>
