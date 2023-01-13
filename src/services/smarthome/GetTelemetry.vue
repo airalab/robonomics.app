c620cb7177480a1b8d05eee5c493fea689754d9a
vol4tim <vol4tim@gmail.com>
2023-01-13 12:07:05 +0400

    added ipfs gateway selection

--- src/services/smarthome/GetTelemetry.vue
+++ src/services/smarthome/GetTelemetry.vue
@@ -6,11 +6,30 @@
       </robo-card-label>
 
       <robo-card-section>
+        <robo-card-title
+          size="3"
+          offset="x05"
+          tooltip="You may insert your gateway for faster download (e.g. https://gateway.pinata.cloud/). Do it only if you know how it works."
+          tooltipIcon="circle-question"
+        >
+          IPFS gateway (extended)
+        </robo-card-title>
+
         <robo-section offset="x05">
+          <robo-select
+            :options="gateways"
+            :values="gateways"
+            v-model="selectedGateway"
+            block
+            :disabled="selectedIndex >= 0 && log[selectedIndex].load"
+          />
+        </robo-section>
+
+        <robo-section v-if="selectedGateway === 'other'" offset="x05">
           <robo-input
-            label="IPFS gateway (extended)"
-            tip="You may insert your gateway for faster download (e.g. https://gateway.pinata.cloud/). Do it only if you know how it works."
             v-model="gateway"
+            placeholder="Enter gateway url"
+            :disabled="selectedIndex >= 0 && log[selectedIndex].load"
           />
         </robo-section>
 
@@ -20,7 +39,21 @@
             :values="ids"
             v-model="selected"
             block
+            :disabled="selectedIndex >= 0 && log[selectedIndex].load"
           />
+        </robo-section>
+        <robo-section
+          v-if="selectedIndex >= 0 && this.log[this.selectedIndex].hash"
+          offset="x05"
+        >
+          <small>
+            <a
+              :href="`${this.gateway}ipfs/${this.log[this.selectedIndex].hash}`"
+              target="_blank"
+            >
+              {{ `${this.gateway}ipfs/${this.log[this.selectedIndex].hash}` }}
+            </a>
+          </small>
         </robo-section>
 
         <robo-section offset="x05">
@@ -34,7 +67,7 @@
           </robo-button>
           <robo-notification
             v-if="selectedIndex >= 0 && log[selectedIndex].error"
-            title="Error"
+            :title="log[selectedIndex].error"
             type="error"
           />
         </robo-section>
@@ -44,16 +77,27 @@
 </template>
 
 <script>
-import robonomics from "../../robonomics";
-import { catFile } from "../../ipfs";
-import { encodeAddress } from "@polkadot/util-crypto";
 import { u8aToString } from "@polkadot/util";
+import { encodeAddress } from "@polkadot/util-crypto";
+import { catFile } from "../../plugins/ipfs";
+import robonomics from "../../robonomics";
 
 export default {
   props: ["controller"],
   emits: ["telemetry"],
   data() {
     return {
+      gateways: [
+        "other",
+        "https://ipfs.io/",
+        "https://gateway.ipfs.io/",
+        "https://cloudflare-ipfs.com/",
+        "https://cf-ipfs.com/",
+        "https://ipfs-gateway.cloud/",
+        "https://jorropo.net/",
+        "https://gateway.pinata.cloud/"
+      ],
+      selectedGateway: "https://ipfs.io/",
       gateway: "https://ipfs.io/",
       log: [],
       selected: ""
@@ -65,6 +109,13 @@ export default {
   watch: {
     controller() {
       this.read();
+    },
+    selectedGateway() {
+      if (this.selectedGateway === "other") {
+        this.gateway = "";
+      } else {
+        this.gateway = this.selectedGateway;
+      }
     }
   },
   computed: {
@@ -136,22 +187,30 @@ export default {
       }
       this.log[this.selectedIndex].error = false;
       this.log[this.selectedIndex].load = true;
+      let result;
       try {
-        const result = await catFile(
-          this.log[this.selectedIndex].hash,
-          this.gateway
-        );
-        const data = this.decrypt(result);
-        if (data) {
-          this.log[this.selectedIndex].data = data;
-        } else {
-          this.log[this.selectedIndex].data = result;
-        }
-        this.log[this.selectedIndex].load = false;
+        result = await catFile(this.log[this.selectedIndex].hash, this.gateway);
       } catch (error) {
         console.log(error);
         this.log[this.selectedIndex].load = false;
-        this.log[this.selectedIndex].error = true;
+        this.log[this.selectedIndex].error =
+          "Data fetch error. Try changing ipfs gateway.";
+      }
+      if (result) {
+        console.log(result);
+        try {
+          const data = this.decrypt(result);
+          if (data) {
+            this.log[this.selectedIndex].data = data;
+          } else {
+            this.log[this.selectedIndex].data = result;
+          }
+          this.log[this.selectedIndex].load = false;
+        } catch (error) {
+          console.log(error);
+          this.log[this.selectedIndex].load = false;
+          this.log[this.selectedIndex].error = "Data decryption error.";
+        }
       }
       this.$emit("telemetry", this.log[this.selectedIndex].data);
     },
