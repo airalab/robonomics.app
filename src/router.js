9ef96f744078adbca63930e772af52cbcc615714
vol4tim <vol4tim@gmail.com>
2023-03-23 12:56:02 +0400

    add lights up demo

--- src/router.js
+++ src/router.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHashHistory } from "vue-router";
 import ServiceHomeAssistantAccount from "./services/home-assistant-account/Page.vue";
+import ServiceLightsUpDemo from "./services/lights-up-demo/Page.vue";
 import ServiceLightsUp from "./services/lights-up/Page.vue";
 import ServiceSmartHomeTelemetry from "./services/smarthome/Page.vue";
 import CreateAccount from "./views/CreateAccount";
@@ -73,6 +74,26 @@ const routes = [
     path: "/lights-up",
     name: "lights-up",
     component: ServiceLightsUp,
+    meta: {
+      breadcrumbs: [
+        {
+          text: "Services",
+          href: "/#/services"
+        },
+        {
+          text: "Lights up",
+          active: true
+        }
+      ],
+      title: "Lights up!",
+      description:
+        "Set color for smart lamp via Robonomics Subscription - Decentrilized IoT cloud alternative."
+    }
+  },
+  {
+    path: "/lights-up-demo",
+    name: "lights-up-demo",
+    component: ServiceLightsUpDemo,
     meta: {
       breadcrumbs: [
         {
