979a5a6d4e8ecac5374b5856805e8e1cd5daec80
vol4tim <vol4tim@gmail.com>
2023-03-30 18:25:25 +0400

    fix unmount colorpicker

--- src/services/lights-up/ColorPicker.vue
+++ src/services/lights-up/ColorPicker.vue
@@ -77,6 +77,9 @@ export default {
       this.colorLightUp = color.hexString;
       this.$emit("update:modelValue", this.colorPicker.color.hexString);
     });
+  },
+  beforeUnmount() {
+    document.querySelector(".IroColorPicker").remove();
   }
 };
 </script>
