4a3767766aaa8d90bb59e9cfe0add325f996e1dc
vol4tim <vol4tim@gmail.com>
2024-10-09 11:51:15 +0400

    fix update balance

--- src/hooks/useBalance.js
+++ src/hooks/useBalance.js
@@ -5,17 +5,21 @@ export const useBalance = (account) => {
   const balance = ref(null);
   const { isReady, getInstance } = useRobonomics();
   let unsubscribe;
-  watch([account, isReady], async () => {
-    if (unsubscribe) {
-      unsubscribe();
-    }
-    if (!isReady.value) {
-      return;
-    }
-    const robonomics = getInstance();
-    unsubscribe = await robonomics.account.getBalance(account.value, (r) => {
-      balance.value = r.free.sub(r.frozen).toNumber();
-    });
-  });
+  watch(
+    [account, isReady],
+    async () => {
+      if (unsubscribe) {
+        unsubscribe();
+      }
+      if (!isReady.value || !account.value) {
+        return;
+      }
+      const robonomics = getInstance();
+      unsubscribe = await robonomics.account.getBalance(account.value, (r) => {
+        balance.value = r.free.sub(r.frozen).toNumber();
+      });
+    },
+    { immediate: true }
+  );
   return { balance, unsubscribe };
 };
