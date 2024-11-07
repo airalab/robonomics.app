859f320c277d8395258360ba9e8bede49bdd25bd
Anastasiia Bakai <nastin_box@list.ru>
2024-11-08 00:46:24 +0200

    tokens activation

---
+++ src/eth_chains.js
@@ -0,0 +1,14 @@
+const chains =
+    {
+       1: {
+        name: 'Ethereum Mainnet'
+       },
+       2: {
+        name: 'Expanse Network'
+       },
+       11155111: {
+        name: 'Sepolia'
+       }
+    }
+
+export default chains
